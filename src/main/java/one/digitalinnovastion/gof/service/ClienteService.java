b72c3c20925e8b400c834d254a4dde8eafd5c724
Murilo Santos <muriloscunha23@gmail.com>
2023-12-15 02:55:22 -0300

    subindo api

---
+++ src/main/java/one/digitalinnovastion/gof/service/ClienteService.java
@@ -0,0 +1,14 @@
+package one.digitalinnovastion.gof.service;
+
+import one.digitalinnovastion.gof.model.Cliente;
+
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+    Cliente buscarPorId(Long id);
+    void inserir (Cliente cliente);
+    void atualizar(Long id, Cliente cliente);
+    void deletar(Long id);
+
+}
---
+++ src/main/java/one/digitalinnovastion/gof/service/ViaCepService.java
@@ -0,0 +1,15 @@
+package one.digitalinnovastion.gof.service;
+
+import one.digitalinnovastion.gof.model.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(name = "viacep", url = "viacep.com.br/ws")
+public interface ViaCepService {
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+    }
+
